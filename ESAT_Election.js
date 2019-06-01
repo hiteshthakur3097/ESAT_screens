656f76c9e24d581d1ce918e1f7ada939fed029e2
Rahul Thakur <rtrahulthakur5@gmail.com>
2019-06-01 20:41:03 +0530

    First Commit

---
+++ ESAT_Election.js
@@ -0,0 +1,442 @@
+function buttoncolor()
+{
+    document.getElementById('button2').style.background='#28b586';
+}
+
+function buttoncolor1()
+{
+    document.getElementById('button3').style.background='#28b586';
+}
+
+function buttoncolor2()
+{
+    document.getElementById('button1').style.background='#28b586';
+}
+
+function buttoncolorout()
+{
+    document.getElementById('button2').style.background='white';
+    document.getElementById('button3').style.background='white';
+}
+
+function buttoncolorout1()
+{
+    document.getElementById('button1').style.background='white';
+}
+
+
+function show1()
+{
+    var x=document.getElementsByClassName("test");
+    var s=document.getElementById("fs");
+    var a=document.getElementById("oyster1");
+    var b=document.getElementById("cat1");
+    var c=document.getElementById("whale1");
+    var d=document.getElementById("elephant1");
+    var e=document.getElementById("fish1");
+    var f=document.getElementById("wgc");
+    var oyster=document.getElementById("oys");
+    var cat=document.getElementById("catss");
+    var whale=document.getElementById("whales");
+    var elephant=document.getElementById("eleph");
+    var fish=document.getElementById("fiss");
+    var goose=document.getElementById("goos");
+    for(i=0;i<x.length;i++)
+    {
+    if(x[i].style.display==="none")
+    {
+        x[i].style.display="block";
+        a.style.display="block";
+        s.style.display="block";
+    }
+    else
+    {
+        x[i].style.display="block";
+    }
+    }
+   
+    if(a.style.display==="none")
+    {
+        a.style.display="block";
+        b.style.display="none";
+        c.style.display="none";
+        d.style.display="none";
+        e.style.display="none";
+        f.style.display="none";
+        s.style.display="block";
+        oyster.style.opacity=1;
+        catss.style.opacity=0.2;
+        whales.style.opacity=0.2;
+        eleph.style.opacity=0.2;
+        fi.style.opacity=0.2;
+        goos.style.opacity=0.2;
+
+    }
+    else
+    {
+        a.style.display="block";
+    }
+
+
+
+}
+function show2()
+{
+    var x=document.getElementsByClassName("test");
+    var s=document.getElementById("fs");
+    var a=document.getElementById("oyster1");
+    var b=document.getElementById("cat1");
+    var c=document.getElementById("whale1");
+    var d=document.getElementById("elephant1");
+    var e=document.getElementById("fish1");
+    var f=document.getElementById("wgc");
+    var oyster=document.getElementById("oys");
+    var cat=document.getElementById("catss");
+    var whale=document.getElementById("whales");
+    var elephant=document.getElementById("eleph");
+    var fish=document.getElementById("fiss");
+    var goose=document.getElementById("goos");
+    for(i=0;i<x.length;i++)
+    {
+    if(x[i].style.display==="none")
+    {
+        x[i].style.display="block";
+    }
+    else
+    {
+        x[i].style.display="block";
+    }
+    }
+
+    if(b.style.display==="none")
+    {
+        a.style.display="none";
+        b.style.display="block";
+        c.style.display="none";
+        d.style.display="none";
+        e.style.display="none";
+        f.style.display="none";
+        s.style.display="block";
+        oyster.style.opacity=0.2;
+        catss.style.opacity=1;
+        whales.style.opacity=0.2;
+        eleph.style.opacity=0.2;
+        fi.style.opacity=0.2;
+        goos.style.opacity=0.2;
+    }
+    else
+    {
+        b.style.display="block";
+    }
+
+
+
+}
+function show3()
+{
+    var x=document.getElementsByClassName("test");
+    var s=document.getElementById("fs");
+    var a=document.getElementById("oyster1");
+    var b=document.getElementById("cat1");
+    var c=document.getElementById("whale1");
+    var d=document.getElementById("elephant1");
+    var e=document.getElementById("fish1");
+    var f=document.getElementById("wgc");
+    var oyster=document.getElementById("oys");
+    var cat=document.getElementById("catss");
+    var whale=document.getElementById("whales");
+    var elephant=document.getElementById("eleph");
+    var fish=document.getElementById("fiss");
+    var goose=document.getElementById("goos");
+    for(i=0;i<x.length;i++)
+    {
+    if(x[i].style.display==="none")
+    {
+        x[i].style.display="block";
+    }
+    else
+    {
+        x[i].style.display="block";
+    }
+    }
+
+    if(c.style.display==="none")
+    {
+        a.style.display="none";
+        b.style.display="none";
+        c.style.display="block";
+        d.style.display="none";
+        e.style.display="none";
+        f.style.display="none";
+        s.style.display="block";
+        oyster.style.opacity=0.2;
+        catss.style.opacity=0.2;
+        whales.style.opacity=1;
+        eleph.style.opacity=0.2;
+        fi.style.opacity=0.2;
+        goos.style.opacity=0.2;
+    }
+    else
+    {
+        c.style.display="block";
+    }
+
+
+
+}
+function show4()
+{
+    var x=document.getElementsByClassName("test");
+    var s=document.getElementById("fs");
+    var a=document.getElementById("oyster1");
+    var b=document.getElementById("cat1");
+    var c=document.getElementById("whale1");
+    var d=document.getElementById("elephant1");
+    var e=document.getElementById("fish1");
+    var f=document.getElementById("wgc");
+    var oyster=document.getElementById("oys");
+    var cat=document.getElementById("catss");
+    var whale=document.getElementById("whales");
+    var elephant=document.getElementById("eleph");
+    var fish=document.getElementById("fiss");
+    var goose=document.getElementById("goos");
+    for(i=0;i<x.length;i++)
+    {
+    if(x[i].style.display==="none")
+    {
+        x[i].style.display="block";
+    }
+    else
+    {
+        x[i].style.display="block";
+    }
+    }
+
+    if(d.style.display==="none")
+    {
+        a.style.display="none";
+        b.style.display="none";
+        c.style.display="none";
+        d.style.display="block";
+        e.style.display="none";
+        f.style.display="none";
+        s.style.display="block";
+        oyster.style.opacity=0.2;
+        catss.style.opacity=0.2;
+        whales.style.opacity=0.2;
+        eleph.style.opacity=1;
+        fi.style.opacity=0.2;
+        goos.style.opacity=0.2;
+    }
+    else
+    {
+        d.style.display="block";
+    }
+
+
+
+}
+function show5()
+{
+    var x=document.getElementsByClassName("test");
+    var s=document.getElementById("fs");
+    var a=document.getElementById("oyster1");
+    var b=document.getElementById("cat1");
+    var c=document.getElementById("whale1");
+    var d=document.getElementById("elephant1");
+    var e=document.getElementById("fish1");
+    var f=document.getElementById("wgc");
+    var oyster=document.getElementById("oys");
+    var cat=document.getElementById("catss");
+    var whale=document.getElementById("whales");
+    var elephant=document.getElementById("eleph");
+    var fish=document.getElementById("fiss");
+    var goose=document.getElementById("goos");
+    for(i=0;i<x.length;i++)
+    {
+    if(x[i].style.display==="none")
+    {
+        x[i].style.display="block";
+    }
+    else
+    {
+        x[i].style.display="block";
+    }
+    }
+
+    if(e.style.display==="none")
+    {
+        a.style.display="none";
+        b.style.display="none";
+        c.style.display="none";
+        d.style.display="none";
+        e.style.display="block";
+        f.style.display="none";
+        s.style.display="block";
+        oyster.style.opacity=0.2;
+        catss.style.opacity=0.2;
+        whales.style.opacity=0.2;
+        eleph.style.opacity=0.2;
+        fi.style.opacity=1;
+        goos.style.opacity=0.2;
+    }
+    else
+    {
+        e.style.display="block";
+    }
+
+
+
+}
+function show6()
+{
+    var x=document.getElementsByClassName("test");
+    var s=document.getElementById("fs");
+    var a=document.getElementById("oyster1");
+    var b=document.getElementById("cat1");
+    var c=document.getElementById("whale1");
+    var d=document.getElementById("elephant1");
+    var e=document.getElementById("fish1");
+    var f=document.getElementById("wgc");
+    var oyster=document.getElementById("oys");
+    var cat=document.getElementById("catss");
+    var whale=document.getElementById("whales");
+    var elephant=document.getElementById("eleph");
+    var fish=document.getElementById("fiss");
+    var goose=document.getElementById("goos");
+    for(i=0;i<x.length;i++)
+    {
+    if(x[i].style.display==="none")
+    {
+        x[i].style.display="block";
+    }
+    else
+    {
+        x[i].style.display="block";
+    }
+    }
+
+    if(f.style.display==="none")
+    {
+        a.style.display="none";
+        b.style.display="none";
+        c.style.display="none";
+        d.style.display="none";
+        e.style.display="none";
+        f.style.display="block";
+        s.style.display="block";
+        oyster.style.opacity=0.2;
+        catss.style.opacity=0.2;
+        whales.style.opacity=0.2;
+        eleph.style.opacity=0.2;
+        fi.style.opacity=0.2;
+        goos.style.opacity=1;
+    }
+    else
+    {
+        f.style.display="block";
+    }
+
+
+
+
+}
+
+function great()
+{
+    var gr=document.getElementById("great");
+    var g=document.getElementById("good");
+    var be=document.getElementById("better");
+    var p=document.getElementById("poor");
+    var but5=document.getElementById("button5")
+    if(gr.style.display==="none")
+    {
+        gr.style.display="block";
+        g.style.display="none";
+        be.style.display="none";
+        p.style.display="none";
+        but5.style.background="#28b586";
+        but5.style.cursor="pointer";
+
+    }
+    else
+    {
+        gr.style.display="block";
+    }
+    
+
+}
+
+function good()
+{
+    var gr=document.getElementById("great");
+    var g=document.getElementById("good");
+    var be=document.getElementById("better");
+    var p=document.getElementById("poor");
+    var but5=document.getElementById("button5")
+    if(g.style.display==="none")
+    {
+        g.style.display="block";
+        gr.style.display="none";
+        be.style.display="none";
+        p.style.display="none";
+        but5.style.background="#28b586";
+        but5.style.cursor="pointer";
+    }
+    else
+    {
+        g.style.display="block";
+    }
+
+}
+
+function better()
+{
+    var gr=document.getElementById("great");
+    var g=document.getElementById("good");
+    var be=document.getElementById("better");
+    var p=document.getElementById("poor");
+    var but5=document.getElementById("button5")
+    if(be.style.display==="none")
+    {
+        be.style.display="block";
+        gr.style.display="none";
+        g.style.display="none";
+        p.style.display="none";
+        but5.style.background="#28b586";
+        but5.style.cursor="pointer";
+    }
+    else
+    {
+        be.style.display="block";
+    }
+
+}
+function poor()
+{
+    var gr=document.getElementById("great");
+    var g=document.getElementById("good");
+    var be=document.getElementById("better");
+    var p=document.getElementById("poor");
+    var but5=document.getElementById("button5")
+    if(p.style.display==="none")
+    {
+        p.style.display="block";
+        gr.style.display="none";
+        g.style.display="none";
+        be.style.display="none";
+        but5.style.background="#28b586";
+        but5.style.cursor="pointer";
+    }
+    else
+    {
+        p.style.display="block";
+    }
+
+}
+
+
+
+
+
+
